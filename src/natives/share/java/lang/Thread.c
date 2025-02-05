16c6090c8f1f02d1b022aee80d1c3a9abf04713a
Max Cai <maxcai.paloalto@gmail.com>
2025-02-05 11:18:11 -0800

    assert that sleep time is non-negative

--- src/natives/share/java/lang/Thread.c
+++ src/natives/share/java/lang/Thread.c
@@ -60,6 +60,8 @@ DECLARE_NATIVE("java/lang", Thread, interrupt0, "()V") {
 
 DECLARE_ASYNC_NATIVE("java/lang", Thread, sleepNanos0, "(J)V", locals(), invoked_methods()) {
   assert(argc == 1);
+  s64 nanos = args[0].l;
+  assert(nanos >= 0); // this is always checked before calling this private method
 
   if (thread->thread_obj->interrupted) {
     thread->thread_obj->interrupted = false; // throw and reset flag
@@ -70,7 +72,7 @@ DECLARE_ASYNC_NATIVE("java/lang", Thread, sleepNanos0, "(J)V", locals(), invoked
   struct timeval tv;
   gettimeofday(&tv, NULL);
   u64 time = tv.tv_sec * 1000000 + tv.tv_usec;
-  u64 end = time + args[0].l / 1000;
+  u64 end = time + nanos / 1000;
 
   rr_wakeup_info *wakeup_info = malloc(sizeof(rr_wakeup_info));
   wakeup_info->kind = RR_WAKEUP_SLEEP;
@@ -84,7 +86,7 @@ DECLARE_NATIVE("java/lang", Thread, clearInterruptEvent, "()V") {
 }
 
 DECLARE_ASYNC_NATIVE("java/lang", Thread, yield0, "()V", locals(), invoked_methods()) {
-  // Thread.yield has no synchronization semantics [JLS 17.9];
+  // Thread.yield has no synchronization semantics (JLS 17.9);
   // the JVM is free to implement it as a no-op or treat it as a scheduling hint.
   // This is usually used to "encourage" more context switches to improve throughput, but
   // "It is rarely appropriate to use this method" (OpenJDK).
