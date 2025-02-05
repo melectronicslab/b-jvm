aa6811d3a957eada9c229c19e0f70b2409ed347b
Max Cai <maxcai.paloalto@gmail.com>
2025-02-05 11:05:51 -0800

    grammar fix :nerd:

--- src/natives/share/java/lang/Thread.c
+++ src/natives/share/java/lang/Thread.c
@@ -86,7 +86,7 @@ DECLARE_NATIVE("java/lang", Thread, clearInterruptEvent, "()V") {
 DECLARE_ASYNC_NATIVE("java/lang", Thread, yield0, "()V", locals(), invoked_methods()) {
   // Thread.yield has no synchronization semantics [JLS 17.9];
   // the JVM is free to implement it as a no-op or treat it as a scheduling hint.
-  // This is usually used to "encourage" more context switches to improve throughput
+  // This is usually used to "encourage" more context switches to improve throughput, but
   // "It is rarely appropriate to use this method" (OpenJDK).
   ASYNC_END_VOID();
 }
