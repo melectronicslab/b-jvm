a17e957a8965bdf3c79a160e609ed71eabe1a153
melectronicslab <>
2025-05-04 21:14:32 +0200

    adding script for initial linking

---
+++ js/demos/scripts/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd src
+ln -s ../../bjvm2.ts lib/bjvm_links/bjvm_interface/bjvm2.ts
+
+ln -s ../../../build/bjvm_main.js lib/bjvm_links/build/bjvm_main.js
+ln -s ../../../build/bjvm_main.d.ts lib/bjvm_links/build/bjvm_main.d.ts
