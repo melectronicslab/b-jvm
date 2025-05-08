a0fbce9d2c5cd16539b76ea73e61f1d01908112d
Melectronics lab <opensource@kazba.eu>
2025-05-08 12:56:11 +0200

    framework loads properly now

--- js/demos/src/lib/util.ts
+++ js/demos/src/lib/util.ts
@@ -9,11 +9,11 @@ export const fetchUint8Array = async (url: string) => {
 	return new Uint8Array(arrayBuffer);
 };
 
-export const loadJar = async (os: BovineOS, name: string) => {
-	const buffer = await fetchUint8Array(`/${name}`);
+export const loadJar = async (os: BovineOS, path: string, name: string) => {
+	const buffer = await fetchUint8Array(`${path}/${name}.jar`);
 	os.FS.mkdir('/rofl', 0o777);
-	os.FS.writeFile(`/rofl/${name}`, buffer);
-	const vm = os.makeVM<any>({ classpath: `/:/rofl/${name}` });
+	os.FS.writeFile(`/rofl/${name}.jar`, buffer);
+	const vm = os.makeVM<any>({ classpath: `/:/rofl/${name}.jar` });
 	const Class = await vm.loadClass(name as any);
 	return Class;
 };
